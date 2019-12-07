f0ad35861e0e8a95e749fdea20acea5b39c78b19
ethonwilles <ethonwilles@gmail.com>
2019-12-06 17:58:30 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import openSocket from "socket.io-client";
+
+import "./main.scss";
 
 function App() {
+  const [msg, setMessage] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [serverMessage, setServerMessage] = React.useState([]);
+  const [rawMessage, setRawMessage] = React.useState("");
+  const [socket, setSocket] = React.useState("");
+
+  React.useEffect(() => {
+    setSocket(openSocket("http://localhost:5000"));
+  }, []);
+
+  const sendMessage = e => {
+    e.preventDefault();
+    setServerMessage([...serverMessage, { msg: rawMessage, id: "recv" }]);
+
+    console.log(msg);
+  };
+  const send = () => {
+    socket.emit("private_chat", {
+      message: rawMessage,
+      username: username,
+      to: "test2"
+    });
+    socket.on("private_chat", data => {
+      setServerMessage([...serverMessage, data.messageRecv, data.messageBack]);
+    });
+    socket.on("private_message", data => {
+      setServerMessage([...serverMessage, data.message]);
+    });
+  };
+  const setChats = () => {
+    return serverMessage.map(elem => {
+      if (elem.id === "recv") {
+        return <p className="userMsg">{elem.msg}</p>;
+      } else if (elem.id === "res") {
+        return <p className="recvMsg">{elem.msg}</p>;
+      }
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="output" id="output">
+        {setChats()}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          placeholder="enter Username"
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="enter message"
+          onChange={e => setRawMessage(e.target.value)}
+        />
+        <button type="submit" onClick={send}>
+          Test
+        </button>
+      </form>
     </div>
   );
 }
